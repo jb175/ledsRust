ddbc38be713e40b460148b82039bff13ba4e1f42
jb <jb.moufle@gmail.com>
2024-12-18 17:11:09 +0100

    ble setup tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{thread, time::Duration};
-use esp_idf_hal::prelude::*;
+use esp_idf_hal::{modem::Modem, prelude::*};
 use esp_idf_sys::*;
 use smart_leds::{SmartLedsWrite, hsv::{Hsv, hsv2rgb}};
 use ws2812_esp32_rmt_driver::Ws2812Esp32Rmt;
@@ -7,7 +7,10 @@ use ws2812_esp32_rmt_driver::Ws2812Esp32Rmt;
 mod animations;
 use animations::{Animation, PulseAnimation};
 
-fn main() {
+mod services;
+use services::bluetooth_service;
+
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -29,10 +32,15 @@ fn main() {
     animation_list.push(Box::new(PulseAnimation::new(Hsv { hue: 120, sat: 255, val: 255 }, Duration::from_millis(500))));
     animation_list.push(Box::new(PulseAnimation::new(Hsv { hue: 240, sat: 255, val: 255 }, Duration::from_millis(500))));
 
+    let bluetooth_service = bluetooth_service::setup();
+
+    log::info!("Continue LedStrips!");
+
     // Run animations
     loop {
         for animation in &animation_list {
             animation.run(&mut ws2812, Duration::from_secs(1));
         }
     }
+    
 }
